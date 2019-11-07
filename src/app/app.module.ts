d4b24060b3184de0103a37941a11116da123611c
Pablo <lezcano12@gmail.com>
2019-11-07 20:33:32 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { PortfolioComponent } from './component/portfolio/portfolio.component';
+import { ContactComponent } from './component/contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
